3bfad3cc932cf8eee4eefacb03804064c9060629
Cesar D. Mendoza <cesar.mendoza@logistyx.com>
2022-04-08 10:50:10 -0500

    Add project files.

---
+++ CDE.MetadataTypeBug/Shared/Models/Collaborator.Partial.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CDE.MetadataTypeBug.Shared.Models
+{
+    // https://docs.microsoft.com/en-us/dotnet/api/system.componentmodel.dataannotations.metadatatypeattribute?view=net-6.0
+    // "The MetadataTypeAttribute attribute enables you to associate a class with a data-model partial class.
+    // In this associated class you provide additional metadata information that is not in the data model.
+    // For example, in the associated class you can apply the RequiredAttribute attribute to a data field."
+
+    [MetadataType(typeof(CollaboratorMetadata))]
+    public partial class Collaborator
+    {
+    }
+
+    public class CollaboratorMetadata
+    {
+        [Required(ErrorMessage = "Last name is required.")]
+        public string LastName { get; set; } = null!;       
+    }
+}
---
+++ CDE.MetadataTypeBug/Shared/Models/Collaborator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace CDE.MetadataTypeBug.Shared.Models
+{
+    // In a real application, this class was auto-generated
+    // (scaffolded using Entity Framework database-first).
+    public partial class Collaborator
+    {
+        public Collaborator()
+        {
+        }
+
+        // This "Required" attribute was added manually
+        // and will be deleted when we need to scaffold it again.
+        [Required(ErrorMessage = "First name is required.")] 
+        public string FirstName { get; set; } = null!;
+
+        public string LastName { get; set; } = null!;
+    }
+}
